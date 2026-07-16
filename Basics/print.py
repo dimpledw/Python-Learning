308290c8448f2b8d97083be244b149611c445859
techindata <dimplew494@gmail.com>
2026-07-16 12:46:01 -0400

    Adding Python Basics Files

---
+++ Basics/print.py
@@ -0,0 +1,13 @@
+print("Hello World")
+
+
+''' Question - 1 '''
+''' Write a program that uses print statements to print the following 
+recipe into the Output console'''
+
+
+print("Mix 100g of Flour, 10g Yeast and 300ml water in a bowl.")
+print("Knead the dough for 10 mins.")
+print("Add 3g of salt.")
+print("Leave to rise for 2 hours.")
+print("Bake at 200 degrees C for 30 minutes.")
---
+++ Basics/string_manipulation.py
@@ -0,0 +1,19 @@
+print("Mix 100g of Flour, 10g Yeast and 300ml water in a bowl.\nKnead the dough for 10 mins.\nAdd 3g of salt.\nLeave to rise for 2 hours.\nBake at 200 degrees C for 30 minutes.")
+
+''' Concatenation of String '''
+
+print("Hello" + " " + "Reader")
+
+''' Question - 2 There are errors on all 5 lines of code. Fix the code so that it runs without errors.'''
+
+''' print(Notes from Day 1")
+ print("The print statement is used to output strings")
+print("Strings are strings of characters"
+priint("String Concatenation is done with the + sign")
+print(("New lines can be created with a \ and the letter n") '''
+
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \ and the letter n")
